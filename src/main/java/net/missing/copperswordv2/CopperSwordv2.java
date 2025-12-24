3a5df5607797260727d99dc25f5d93323f795501
Missing505 <voevaul@gmail.com>
2025-12-24 11:14:07 -0500

    a

---
+++ src/main/java/net/missing/copperswordv2/CopperSwordv2.java
@@ -0,0 +1,15 @@
+package net.missing.copperswordv2;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CopperSwordv2 implements ModInitializer {
+	public static final String MOD_ID = "copperswordv2";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/missing/copperswordv2/CopperSwordv2Client.java
@@ -0,0 +1,10 @@
+package net.missing.copperswordv2;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperSwordv2Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
